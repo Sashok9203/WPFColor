798db0a735469668e4d8ebc22097c6e2a0f1499c
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-06-09 16:48:07 +0300

    BindingUpdate

--- WPFColor/MainWindow.xaml.cs
+++ WPFColor/MainWindow.xaml.cs
@@ -9,59 +9,29 @@ using PropertyChanged;
 
 using System.Windows.Media;
 using System.Windows.Data;
+using System.Collections.ObjectModel;
 
 namespace WpfApp2
 {
     public partial class MainWindow : Window
     {
-
-        MyView view ;
+        MyViewModel view ;
         public MainWindow()
         {
-           
+          
             InitializeComponent();
-            view = new(colorsList);
+            view = new();
+            colorsList.ItemsSource = view.CList;
             this.DataContext = view;
         }
 
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
-            if ((sender as Button)?.Content.ToString() == "Add")
-                colorsList.Items.Add(view.CurrentColorHex);
-            else
-                colorsList.Items.RemoveAt(colorsList.SelectedIndex);
-            view.ItemCount = colorsList.Items.Count;
+            if ((sender as Button)?.Content.ToString() == "Add") view.AddHexString();
+            else  view.RemoveHexString(colorsList.SelectedIndex);
         }
     }
 
-    [AddINotifyPropertyChangedInterface]
-    public class MyView
-    {
-        private ListBox colorList;
-        public double Alpha { get; set; }
-        public double Red { get; set; }
-        public double Green { get; set; }
-        public double Blue { get; set; }
-        public int SelectedIndex { get; set; }
-        public int ItemCount { get; set; }
-
-
-        public MyView(ListBox list) 
-        {
-            Alpha = Red = Green = Blue = 0;
-            colorList = list;
-        }
-
-        [DependsOn("alpha", "red", "green", "blue")]
-        public SolidColorBrush CurrentColor => new(Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue));
-
-        [DependsOn("SelectedIndex")]
-        public bool IsSelected => ItemCount != 0 && SelectedIndex >= 0;
-
-        [DependsOn("CurrentColor", "ItemCount")]
-        public bool IsNotColorExist => !colorList.Items.Contains(CurrentColorHex);
-
-        public string CurrentColorHex => $"#{(byte)Alpha:X2}{(byte)Red:X2}{(byte)Green:X2}{(byte)Blue:X2}";
-    }
+   
 
 }
---
+++ WPFColor/MyViewModel.cs
@@ -0,0 +1,56 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp2
+{
+    [AddINotifyPropertyChangedInterface]
+    internal class MyViewModel
+    {
+
+        private ObservableCollection<string> ColorList;
+        public IEnumerable<string> CList => ColorList;
+        public double Alpha { get; set; }
+        public double Red { get; set; }
+        public double Green { get; set; }
+        public double Blue { get; set; }
+        public int SelectedIndex { get; set; }
+        public int ItemsCount { get; set; }
+
+
+        public MyViewModel()
+        {
+            ColorList = new();
+            Alpha = Red = Green = Blue = 0;
+        }
+
+        [DependsOn("alpha", "red", "green", "blue")]
+        public SolidColorBrush CurrentColor => new(Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue));
+
+        [DependsOn("SelectedIndex")]
+        public bool IsSelected =>  SelectedIndex >= 0;
+
+        [DependsOn("CurrentColor", "ItemsCount")]
+        public bool IsNotColorExist => !ColorList.Contains(CurrentColorHex);
+
+        public string CurrentColorHex => $"#{(byte)Alpha:X2}{(byte)Red:X2}{(byte)Green:X2}{(byte)Blue:X2}";
+
+        public void AddHexString() 
+        {
+            ColorList.Add(CurrentColorHex);
+            ItemsCount = ColorList.Count;
+        }
+
+        public void RemoveHexString(int index)
+        {
+            ColorList.RemoveAt(index);
+            ItemsCount = ColorList.Count;
+        } 
+    }
+}
