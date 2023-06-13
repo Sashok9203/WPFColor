fc8913879e772de434aa38bd11991aa778d8200d
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-06-13 19:32:33 +0300

    Update

--- WPFColor/MyViewModel.cs
+++ WPFColor/MyViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -14,30 +15,91 @@ namespace WpfApp2
     [AddINotifyPropertyChangedInterface]
     internal class MyViewModel
     {
+        private int index;
+        private int oldIndex;
+        private Color tmpCColor;
+
         private readonly RelayCommand addColor;
         private readonly RelayCommand delColor;
-        private readonly ObservableCollection<string> ColorList;
-        public IEnumerable<string> CList => ColorList;
+        private readonly RelayCommand clearSelection;
+        private readonly ObservableCollection<Color> ColorList;
+          
+        public IEnumerable<Color> CList => ColorList;
         public double Alpha { get; set; }
         public double Red { get; set; }
         public double Green { get; set; }
         public double Blue { get; set; }
-        public int SelectedIndex { get; set; }
-       
+
+        [DependsOn("SelectedIndex")]
+        public Visibility HelpVisible => SelectedIndex >= 0? Visibility.Visible: Visibility.Hidden;
+        
+
+        
+
+        public int SelectedIndex 
+        {
+            get
+            {
+                if (index >= 0 && index != oldIndex)
+                {
+                    Alpha = ColorList[index].A;
+                    Red = ColorList[index].R;
+                    Green = ColorList[index].G;
+                    Blue = ColorList[index].B;
+                }
+                oldIndex = index;
+                return index;
+            }
+            set
+            {
+                index = value;
+                //if (index >= 0)
+                //{
+                //    Alpha = ColorList[index].A;
+                //    Red = ColorList[index].R;
+                //    Green = ColorList[index].G;
+                //    Blue = ColorList[index].B;
+                //}
+            }
+        }
+
+        
+
+        
+
 
         public ICommand AddColor => addColor;
         public ICommand DelColor => delColor;
-
+        public ICommand ClearSelection => clearSelection;
 
         public MyViewModel()
         {
+            index = -1;
             ColorList = new();
-            Alpha = Red = Green = Blue = 0;
-            addColor = new((o) => { ColorList.Add(CurrentColor.ToString()); ; }, (o) => !ColorList.Contains(CurrentColor.ToString()) );
-            delColor = new((o) => { ColorList.RemoveAt(SelectedIndex);}, (o) =>  SelectedIndex >= 0);
+            addColor = new((o) => { ColorList.Add(tmpCColor); }, (o) => !ColorList.Contains(tmpCColor));
+            delColor = new((o) => { ColorList.RemoveAt(SelectedIndex); }, (o) => SelectedIndex >= 0);
+            clearSelection = new((o) => { SelectedIndex = -1; }, (o) => SelectedIndex >= 0);
         }
 
         [DependsOn("alpha", "red", "green", "blue")]
-        public SolidColorBrush CurrentColor => new(Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue));
+        public Color CurrentColor
+        {
+            get
+            {
+                tmpCColor = Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue);
+
+                if (SelectedIndex >= 0)
+                {
+                    int tmp = SelectedIndex;
+                    ColorList[SelectedIndex] = tmpCColor;
+                    SelectedIndex = tmp;
+                }
+                
+                return tmpCColor;
+            }
+         }
+        
+        
+
     }
 }
